3ec72a6b2273983238bb0c0bcaf5f59465608f6a
Joel Kelly <joel.kelly@afgonline.com.au>
2019-07-26 10:57:27 +0800

    fixed naming

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="AWS Cred Handler",
+    name="cred_handler",
     version="1",
     author_email="joel.kelly@afgonline.com.au",
     description="Decrypt creds from S3 Bucket using KMS",
-    py_modules=['app']
+    py_modules=['cred_handler']
 )
